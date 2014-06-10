ecaf67a545e3e265f1c7ce6987ce9d4fba125e53
Leo Bärring <leo.barring@gmail.com>
2014-06-10 16:04:36 +0200

    instructions added
    Signed-off-by: Leo Bärring <leo.barring@gmail.com>

---
+++ how_to_think_with_portals.sh
@@ -0,0 +1,35 @@
+# Welcome
+Welcome to the aperture command line enrichment interface.
+
+## How to get hold of the portalgun.sh
+You need to `source portalgun.sh`, in order to be able
+to use it. Once you hold the portalgun.sh you can either
+shoot `blue` (or `orange`) portals at your current
+location or aim it somewhere else as in `blue somedir`
+(or `orange somedir`).
+
+You can remove either portal with `rmblue` or `rmorange`.
+and jump through portals with `jump`. (The jump command
+is just a fancier cd, with some extra stuff like helping
+reorient you after the spatial confusion inflicted by
+teleportation.)
+
+The participant should remember that while the portals
+are persistent, the knowledge of the location of the
+portals are not, so at the end of each test the
+participant would do well to remove any and all open portals
+to avoid unnecesary side effects.
+
+## Caveat Emptor
+The aperture command line enrichment interface testing
+and monitoring department would like you to know that
+related and/or unrelated effects such as, but not limited
+to, temporal instability, impromptu games of exploding
+musical chairs, irreversible loss of data, file system
+corruption, and/or coffee supply depletion, are all to be
+considered and logged as part of the test. If the subject,
+or the subject's boss, consider any such and other effects
+to be unacceptable, the aperture command line enrichment
+interface and monitoring department would like to remind
+you that participation in the test is entirely optional,
+and that it is perfectly fine to run away if you are scared.
